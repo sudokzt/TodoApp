7305eb6546b7a4339c2a76ae65fe2d45e6792e1a
k.mishima <k.mishima@kyotoinnovation.jp>
2019-04-20 01:40:42 +0900

    heroku deploy更新

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 //app.js
 const express = require("express");
-const http = require("http");
 const path = require("path");
 let app = express();
 app.use(express.static(path.join(__dirname, "build")));
 const port = process.env.PORT || "8080";
 app.set("port", port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
-console.log("HelloWorld");
+
+const router = express.Router();
+
+// GET /api/v1/article/test
+router.get("/", (req, res) => {
+  res
+    .render("../index")
+    .listen(port, () => console.log(`Running on localhost:${port}`));
+  res.json({
+    message: "This is article api"
+  });
+});
