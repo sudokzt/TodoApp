b4553930e2e27c0b0c82e446ca73a33f08998663
k.mishima <k.mishima@kyotoinnovation.jp>
2019-04-20 01:10:14 +0900

    heroku deploy更新

--- server/app.js
+++ server/app.js
@@ -8,3 +8,4 @@ const port = process.env.PORT || "8080";
 app.set("port", port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+console.log("HelloWorld");
